9bb732c47f66e79a151012be1c227d6250cb0284
kedixa <1204837541@qq.com>
2016-10-27 11:23:55 +0800

    add push(timeout), pop(timeout)

--- k_queue.hpp
+++ k_queue.hpp
@@ -8,61 +8,81 @@
 #include <mutex>
 #include <condition_variable>
 #include <type_traits>
+#include <chrono>
+#include <cassert>
 
 template<typename T>
 class k_queue
 {
+    using u_lock = std::unique_lock<std::mutex>;
+    using milli_seconds = std::chrono::milliseconds;
 private:
     std::queue<T> que;
     std::mutex mtx;
-    std::condition_variable cv;
+    std::condition_variable cv_not_full;
+    std::condition_variable cv_not_empty;
     size_t max_size;
+    bool stop_flag;
 public:
-    k_queue(int max_size = std::numeric_limits<size_t>::max())
-        : max_size(max_size) {}
+    k_queue(size_t max_size = std::numeric_limits<size_t>::max())
+    {
+        stop_flag = false;
+        this->max_size = max_size;
+    }
     k_queue(k_queue<T>&) = delete;
     k_queue operator=(k_queue<T>&) = delete;
 
-    std::pair<bool, T> pop()
+    std::pair<bool, T> pop(milli_seconds milli = milli_seconds(0))
     {
-        std::unique_lock<std::mutex> lk(mtx);
-        std::pair<bool, T> result;
-        if(que.empty()) result = std::make_pair(false, T());
-        else
+        u_lock lk(mtx);
+        std::pair<bool, T> result = std::make_pair(false, T());
+        if(cv_not_empty.wait_for(lk, milli, [&](){return stop_flag || !que.empty();}))
         {
+            if(stop_flag) return result; // stop
+            assert(!que.empty()); // DEBUG
             result = std::make_pair(true, std::move(que.front()));
             que.pop();
+            if(que.size() != max_size)
+                cv_not_full.notify_one();
         }
         return result;
     }
     T pop_block()
     {
-        std::unique_lock<std::mutex> lk(mtx);
-        auto f = [&](){return !que.empty();};
-        cv.wait(lk, f);
+        u_lock lk(mtx);
+        cv_not_empty.wait(lk, [&](){return stop_flag || !que.empty();});
+        if(stop_flag) return T(); // the function requires a return value, but wrong
+        assert(!que.empty()); // DEBUG
         auto result = std::move(que.front());
         que.pop();
+        if(que.size() != max_size)
+            cv_not_full.notify_one();
         return result;
     }
     template<typename U>
     typename std::enable_if<
             std::is_same<T, /*typename is important*/typename std::remove_reference<U>::type>::value, bool
         >::type
-    push(U&& t)
+    push(U&& t, milli_seconds milli = milli_seconds(0))
     {
-        std::unique_lock<std::mutex> lk(mtx);
-        if(que.size() == max_size) return false;
-        else
+        u_lock lk(mtx);
+        if(cv_not_full.wait_for(lk, milli, [&](){return stop_flag || que.size() != max_size;}))
         {
+            if(stop_flag) return false;
+            assert(que.size() != max_size);
             que.push(std::forward<T>(t));
-            lk.unlock();
-            cv.notify_one();
+            cv_not_empty.notify_one();
+            return true;
         }
-        return true;
+        else return false;
     }
     bool empty()
     {
         return que.empty();
+    }
+    bool full()
+    {
+        return que.size() == max_size;
     }
     size_t size()
     {
--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ class cc
     public:
     cc(){}
 };
-k_queue<cc> que(3);
+k_queue<cc> que(1);
 const int sz = 10;
 
 int main()
@@ -16,9 +16,13 @@ int main()
     thread t1 = thread([&](){
         for(int i = 0; i < sz; i++)
         {
-            auto t = que.pop();
+            auto t = que.pop(chrono::milliseconds(0));
             if(t.first) cout<<"thread 1 pop: "<<endl;
-            else --i;
+            else 
+            {
+                cout<<"wait"<<endl;
+                --i;
+            }
         }
     });
     thread t2 = thread([&](){
@@ -26,7 +30,11 @@ int main()
         {
             auto f=que.push(cc());
             if(f)cout<<"thread 2 push"<<endl;
-            else --i;
+            else
+            {
+                cout<<"wait 2"<<endl;
+                --i;
+            }
         }
     });
     t1.join();
