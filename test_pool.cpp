93571e886c6e930228c8c6a96fa50af9eef16243
kedixa <1204837541@qq.com>
2016-11-06 23:37:42 +0800

    test thread pool

--- test_pool.cpp
+++ test_pool.cpp
@@ -1,28 +1,106 @@
-#include "k_thread_pool.hpp"
+#include "thread_pool.hpp"
 #include <iostream>
 #include <sstream>
 #include <thread>
+#include <chrono>
+#include <cassert>
 using namespace std;
+using kedixa::thread_pool;
+const int sz = 1000000;
 
-void f(int x)
+void func(int x)
 {
     ostringstream oss;
     oss<<this_thread::get_id()<<" "<<x<<endl;
     cout<<oss.str();
 }
-int main()
+
+void test_work()
 {
-    k_thread_pool pool(3);
+    thread_pool pool(3);
+    cout << "test work:\n";
     thread th1 = thread([&](){
-        for(int i = 0; i < 10; i++)
-            pool.add(f, i);
+        for(int i = 0; i < 8; i+=2)
+            while(!pool.add(func, i));
     });
     thread th2 = thread([&](){
-        for(int i = 0; i < 10; i++)
-            pool.add(f, 30 - i);
+        for(int i = 1; i < 8; i+=2)
+            while(!pool.add(func, i));
     });
     th1.join();
     th2.join();
     pool.wait();
+    cout << endl;
+}
+void test_wait_stop()
+{
+    thread_pool pool1(3);
+    cout << "test wait:\n";
+    for(int i = 0; i < 8; i++)
+        pool1.add(func, i);
+    pool1.wait();
+
+    cout << "test stop:\n";
+    thread_pool pool2(3);
+    for(int i = 0; i < 8; i++)
+        pool2.add(func, i);
+    for(int i = 0; i < 1000000; i++);
+    pool2.stop();
+    cout << endl;
+}
+void vf(){}
+void test_functions()
+{
+    thread_pool pool(3);
+    cout << "test functions:\n";
+    pool.add([](){}); // lambda
+    pool.add(vf); // function
+    pool.add(&vf); // function pointer
+    pool.add(negate<int>(), -1); // function object
+    cout << "passed without error.\n";
+    cout << endl;
+}
+
+void test_ref()
+{
+    cout << "test reference:\n";
+    int x = 0;
+    auto f = [](int &y){y++;};
+    thread_pool pool1(1);
+    pool1.add(f, x);
+    pool1.wait();
+    assert(x == 0);
+
+    thread_pool pool2(1);
+    pool2.add(f, ref(x));
+    pool2.wait();
+    assert(x == 1);
+    cout << "passed without error.\n";
+    cout << endl;
+}
+
+void test_efficiency()
+{
+    cout << "test efficiency:\n";
+    using chrono::system_clock;
+    auto f = [](){};
+    thread_pool pool1;
+    auto start = system_clock::now();
+    for(int i = 0; i < sz; ++i)
+        pool1.add(f);
+    pool1.wait();
+    auto end = system_clock::now();
+    auto milliseconds = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cout << milliseconds.count() << " milliseconds used for run " << sz <<
+        " empty functions.\n";
+    cout << endl;
+}
+int main()
+{
+    test_work();
+    test_wait_stop();
+    test_functions();
+    test_ref();
+    test_efficiency();
     return 0;
 }
--- thread_pool.hpp
+++ thread_pool.hpp
@@ -2,26 +2,33 @@
     Author: kedixa
     E-mail: 1204837541@qq.com
     License: GPL2
+
+    class kedixa::thread_pool
+    Description:
+        Thread pool for processing multi events.
 */
 
 #include <thread>
-#include "k_queue.hpp"
+#include <chrono>
+#include "queue.hpp"
 
-class k_thread_pool
+namespace kedixa
+{
+class thread_pool
 {
 private:
     size_t thread_num; // the number of threads.
     bool stop_running;
     bool stop_add;
-    k_queue<std::function<void()>> task_que;
+    queue<std::function<void()>> task_que;
     std::vector<std::shared_ptr<std::thread>> threads;
-    std::once_flag one_flag;
+    std::once_flag one_flag; // join_all_threads can only call once.
     
     /*
      * Add std::function<void()> to task_que,
      * only called by member functions.
      */
-    bool _add(std::function<void()> &&fn)
+    bool _add(std::function<void()> &&fn) noexcept(noexcept(task_que.push(fn)))
     {
         if(stop_add) return false;
         return task_que.push(fn);
@@ -42,14 +49,14 @@ private:
     /*
      * Wait all threads to return.
      */
-    void join_all_threads()
+    void join_all_threads() noexcept
     {
         for(auto &t : threads)
             if(t) t->join();
         threads.clear();
     }
 public:
-    k_thread_pool(size_t thread_num =
+    thread_pool(size_t thread_num =
         std::thread::hardware_concurrency()) noexcept
     {
         stop_running = false;
@@ -57,24 +64,22 @@ public:
         this->thread_num = thread_num;
         for(size_t i = 0; i < this->thread_num; ++i)
             threads.push_back(std::make_shared<std::thread>(
-                &k_thread_pool::run, this));
+                &thread_pool::run, this));
     }
 
     /*
      * Add task function(args) to task_que,
-     * enable only if fn is a function.
+     * enable only if fn is a callable object.
      */
     template<class Fn, class... Args>
-    typename std::enable_if<std::is_function<Fn>::value, bool>::type
-    add(Fn &fn, Args&&... args)
+    bool add(Fn &fn, Args&&... args)
     {
-        _add(std::bind(fn, args...));
+        return _add(std::bind(fn, args...));
     }
     template<class Fn, class... Args>
-    typename std::enable_if<std::is_function<Fn>::value, bool>::type
-    add(Fn &&fn, Args&&... args)
+    bool add(Fn &&fn, Args&&... args)
     {
-        _add(std::bind(std::forward<Fn>(fn), args...));
+        return _add(std::bind(std::forward<Fn>(fn), args...));
     }
 
     /*
@@ -98,13 +103,15 @@ public:
     void wait()
     {
         stop_add = true;
+        task_que.wait();
         std::call_once(one_flag, [this]{join_all_threads();});
         stop_running = true;
         task_que.stop();
     }
 
-    ~k_thread_pool()
+    ~thread_pool() noexcept
     {
         stop();
     }
 };
+} // namespace kedixa
